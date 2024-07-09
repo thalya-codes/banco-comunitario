1028ba27cb0312e16017f4e3b652ca90b8c60ab6
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-07-09 19:55:12 -0300

    feat: adiciona novas constantes

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,6 @@
 export const INSUFICIENT_BALANCE = 'Insuficient balance';
+export const EXISTENT_CLIENT = 'Client already registered';
+export const NOT_EXISTENT_CLIENT = 'Client not registered';
+export const INSUFICIENT_INCOME_SALARY =
+  'To open a transaction account, it is necessary to have a income salary of at least R$500.00';
+export const NOT_FOUND_ACCOUNT = 'The given account not exists';

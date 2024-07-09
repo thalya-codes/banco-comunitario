2f93ce6a9d1906d0c4f7674a1360f13621edc2b9
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-07-09 16:38:16 -0300

    feat: cria classe abstrata Account

---
+++ src/account/account.model.ts
@@ -0,0 +1,18 @@
+export abstract class Account {
+  protected _balance: number = 0.0;
+  protected _accountType: string;
+  protected _clientId: string;
+  protected _accountNumber: string;
+
+  abstract deposit(amount: number): void;
+  abstract withdraw(amount: number): void;
+  abstract verifyBalance(): number;
+  abstract transfer(destination: Account, amount: number): void;
+  abstract generateAccountNumber(): string;
+
+  abstract set accountType(newType: string);
+  abstract get accountType(): string;
+  abstract get clientId(): string;
+  abstract get accountNumber(): string;
+  abstract set accountNumber(accountNumber: string);
+}

7c51f8b48fec0642e877c4123d37707334859142
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-07-09 22:46:47 -0300

    feat: agrupa testes do  TransationAccount

---
+++ src/transation-account/test/transation-account.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TransationAccountController } from '../transation-account.controller';
+
+describe('TransationAccountController', () => {
+  let controller: TransationAccountController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TransationAccountController],
+    }).compile();
+
+    controller = module.get<TransationAccountController>(
+      TransationAccountController,
+    );
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
